a4559647bedbd42e80760978b6e3b15f3e3e73de
lu <a0960753785@gmail.com>
2025-08-17 01:46:33 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+console.log('Hello world');
+
+// 取得網址參數
+  const urlParams = new URLSearchParams(window.location.search);
+  const isLoggedInParam = urlParams.get('isLoggedIn'); // 取得 isLoggedIn 的值，會是字串或 null
+
+  const userAvatar = document.getElementById('user-avatar');
+  const guestMessage = document.getElementById('guest-message');
+
+  // 判斷是否登入，參數是 "true" 字串才算登入
+  const isLoggedIn = isLoggedInParam === 'true';
+
+  if (isLoggedIn) {
+    userAvatar.classList.remove('d-none');
+    guestMessage.classList.add('d-none');
+  } else {
+    userAvatar.classList.add('d-none');
+    guestMessage.classList.remove('d-none');
+  }
