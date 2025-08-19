3d578eab82b139dad1c390e1b0d4d8db89a8eadc
lu <a0960753785@gmail.com>
2025-08-20 01:59:40 +0800

    新增首頁menu按鈕功能頁面

--- main.js
+++ main.js
@@ -8,17 +8,29 @@ console.log('Hello world');
   const isLoggedInParam = urlParams.get('isLoggedIn'); // 取得 isLoggedIn 的值，會是字串或 null
 
   const userAvatar = document.getElementById('user-avatar');
+  const offcanvasuserAvatar = document.getElementById('offcanvas-user-avatar');
+  const offcanvasuserAvatarBtn = document.getElementById('offcanvas-user-avatar-btn');
   const guestMessage = document.getElementById('guest-message');
+  const offcanvasGuestMessage = document.getElementById('offcanvas-guest-message');
+  const offcanvasGuestMessageBtn = document.getElementById('offcanvas-guest-message-btn');
 
   // 判斷是否登入，參數是 "true" 字串才算登入
   const isLoggedIn = isLoggedInParam === 'true';
 
   if (isLoggedIn) {
     userAvatar.classList.remove('d-none');
+    offcanvasuserAvatar.classList.remove('d-none');
+    offcanvasuserAvatarBtn.classList.remove('d-none');
     guestMessage.classList.add('d-none');
+    offcanvasGuestMessage.classList.add('d-none');
+    offcanvasGuestMessageBtn.classList.add('d-none');
   } else {
     userAvatar.classList.add('d-none');
+    offcanvasuserAvatar.classList.add('d-none');
+    offcanvasuserAvatarBtn.classList.add('d-none');
     guestMessage.classList.remove('d-none');
+    offcanvasGuestMessage.classList.remove('d-none');
+    offcanvasGuestMessageBtn.classList.remove('d-none');
   }
 
    var swiper = new Swiper(".mySwiper", {
